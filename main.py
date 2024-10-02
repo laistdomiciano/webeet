f20c482090f3952267881694d39ff92f4c870c61
laistdomiciano <laistdomiciano@gmail.com>
2024-10-02 11:40:57 +0200

    First Methods

--- main.py
+++ main.py
@@ -7,3 +7,26 @@ with open('characters.json', 'r') as file:
     characters = json.load(file)
 
 
+#Fetch all characters (with Pagination) - 10 Points
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    pass
+
+
+#Feature 2: Fetch a specific character by ID - 5 Points
+@app.route('/characters/<int:id>', methods=['GET'])
+def get_character_by_id(id):
+    pass
+
+#Feature 3: Fetch a fIltered character list - 10 Points
+
+
+#Feature 4: Fetch a sorted character list - 10 Points
+
+
+#Feature 5: Add a new character to the list - 25 points
+
+#Feature 6: Edit a character - 30 points
+
+#Feature 7: Delete a character - 10 points
+
