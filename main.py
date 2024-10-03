266e09d7cf0f0b6970b8908015da8b1894070c93
laistdomiciano <laistdomiciano@gmail.com>
2024-10-03 07:26:29 +0200

    Organized Needed Endpoints

--- main.py
+++ main.py
@@ -19,14 +19,31 @@ def get_character_by_id(id):
     pass
 
 #Feature 3: Fetch a fIltered character list - 10 Points
+@app.route('/characters/filter', methods=['GET'])
+def filter_characters():
+    pass
 
 
 #Feature 4: Fetch a sorted character list - 10 Points
+@app.route('/characters/sorted', methods=['GET'])
+def sorted_characters():
+    pass
 
 
 #Feature 5: Add a new character to the list - 25 points
+@app.route('/characters', methods=['POST'])
+def add_character():
+    pass
+
 
 #Feature 6: Edit a character - 30 points
+@app.route('/characters/<int:id>', methods=['PATCH'])
+def edit_character(id):
+    pass
+
 
 #Feature 7: Delete a character - 10 points
+@app.route('/characters/<int:id>', methods=['DELETE'])
+def delete_character(id):
+    pass
 
