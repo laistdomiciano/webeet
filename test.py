d31580156749093b0d46de292c4c72128bc338e7
laistdomiciano <laistdomiciano@gmail.com>
2024-10-09 15:59:30 +0200

    another test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 
 BASE_URL = "http://localhost:5000"
 
-def test_fetch_all_characters():
+def test_all_characters():
     response = requests.get(f"{BASE_URL}/characters?limit=20")
     assert response.status_code == 200
     data = response.json()
@@ -11,17 +11,26 @@ def test_fetch_all_characters():
     assert len(data) <= 20
 
 
-def test_fetch_specific_character():
+def test_specific_character():
     response = requests.get(f"{BASE_URL}/characters/1")
     assert response.status_code == 200
     data = response.json()
     assert data['id'] == 1
 
 
-def test_fetch_filtered_characters():
+def test_filtered_characters():
     response = requests.get(f"{BASE_URL}/characters?house=stark&age_more_than=20")
     assert response.status_code == 200
     data = response.json()
     for character in data:
         assert character['house'].lower() == 'stark'
         assert character['age'] >= 20
+
+
+def test_sorted_characters():
+    response = requests.get(f"{BASE_URL}/characters?sort_asc=name")
+    assert response.status_code == 200
+    data = response.json()
+    names = [character['name'] for character in data]
+    assert names == sorted(names)
+
